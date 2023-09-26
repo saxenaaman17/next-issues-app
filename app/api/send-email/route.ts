db06946b77ea727d48771803ae8b220316f42b52
aman28 <aman@praemineo.com>
2023-09-27 02:51:02 +0530

    comment send email api route code

--- app/api/send-email/route.ts
+++ app/api/send-email/route.ts
@@ -1,16 +1,16 @@
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
-// import WelcomeTemplate from "@/emails/WelcomeTemplate";
+// import { NextResponse } from "next/server";
+// import { Resend } from "resend";
+// // import WelcomeTemplate from "@/emails/WelcomeTemplate";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  await resend.emails.send({
-    from: "...",
-    to: "saxenaaman17@gmail.com",
-    subject: "...",
-    react: "pass your react component here (without the quotes)",
-  });
+// export async function POST() {
+//   await resend.emails.send({
+//     from: "...",
+//     to: "saxenaaman17@gmail.com",
+//     subject: "...",
+//     react: "pass your react component here (without the quotes)",
+//   });
 
-  return NextResponse.json({});
-}
+//   return NextResponse.json({});
+// }
